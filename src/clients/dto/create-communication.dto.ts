6b84f713466f560fa80a2add2c2767fc3d42a4b0
iobox420 <iobox420@gmail.com>
2023-06-06 17:14:30 +0300

    feat: add complete validation in communication table

--- src/clients/dto/create-communication.dto.ts
+++ src/clients/dto/create-communication.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEnum, IsMobilePhone, IsString, Validate } from "class-validator";
-import { isEnumEmailPhoneValidator } from "src/shared/isEnumValidator";
+import { IsDefined, IsEnum, IsMobilePhone, IsString } from "class-validator";
 
 export class CreateCommunicationDto {
     @ApiProperty({
@@ -10,11 +9,12 @@ export class CreateCommunicationDto {
     * phone - Мобильный телефон`,
         enum: ['email', 'phone']
     })
+    @IsDefined({ message: 'type - должен быть not be null or undefined' })
     @IsEnum(['email','phone'],{ message: 'type - должно быть enum - email | phone' })
-
     readonly type?: string;
 
     @ApiProperty({ example: '79821048575', description: 'Значение средства связи' }) 
+    @IsDefined({ message: 'value - должен быть not be null or undefined' })
     @IsString({ message: 'value - должно быть строкой' })
     @IsMobilePhone()
     readonly value?: string;
